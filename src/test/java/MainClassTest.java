06bf6753f077a93864825ed4daf26fb30ee39e91
AravindNaik21 <d.aravind.naik@gmail.com>
2026-01-20 12:57:00 +0530

    B

---
+++ src/test/java/MainClassTest.java
@@ -0,0 +1,12 @@
+package com.wip;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainClassTest {
+    @Test
+    public void testAddition() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
