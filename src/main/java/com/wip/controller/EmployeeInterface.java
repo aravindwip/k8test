5a8b2ec377e5e846e61cd46dfa0631130008f994
AravindNaik21 <d.aravind.naik@gmail.com>
2026-01-20 12:25:41 +0530

    m

---
+++ src/main/java/com/wip/controller/EmployeeInterface.java
@@ -0,0 +1,16 @@
+package com.wip.controller;
+
+public interface EmployeeInterface {
+    void addEmployee();
+    void viewEmployee();
+    void addEmployeeToArray();
+    void sortByEmpId();
+    void sortByEname();
+    void sortByEidUsingComparator();
+    void serializeEmp();
+    void deserializeEmp();
+    void updateEmployee();
+    void deleteEmployee();
+    void insertEmployeeViaProc();
+    void getDBinfo();
+}
